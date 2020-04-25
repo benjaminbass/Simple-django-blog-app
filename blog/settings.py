0ad0c9669f0ab535e6ee032d43c2afbacbbfcae5
Bendoo1 <bendoo12345@gmail.com>
2020-04-25 12:59:05 +0000

    added heroku and postgres support

--- blog/settings.py
+++ blog/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 
-if "DATASE_URL" in os.environ:
+if "DATABASE_URL" in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
